44518c5352d6762641989e043cdfe7fe3066f854
Akid0-10 <n.akid89@gmail.com>
2021-03-19 00:15:25 +0800

    Second Commit

--- Lab1/src/Exercise7/Weight.java
+++ Lab1/src/Exercise7/Weight.java
@@ -1,7 +1,5 @@
 package Exercise7;
 
-import java.text.DecimalFormat;
-
 public class Weight {
 
 	private double weight;
@@ -18,18 +16,4 @@ public class Weight {
 		return (weight/9.81)*3.711;
 	}
 	
-	public static void main(String[] args) {
-		
-		DecimalFormat df = new DecimalFormat("0.00");
-		
-		Weight weight1 = new Weight();
-		weight1.setWeight(50);
-		
-		System.out.println("Weight on Earth: "+ 
-		df.format(weight1.getWeight())+ " kg");
-		
-		System.out.println("Weight on Mars: "+ 
-		df.format(weight1.calculateWeight())+ " kg");
-	}
-
 }
